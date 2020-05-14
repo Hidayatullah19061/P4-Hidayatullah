5653e206cb20a8300035a6cf8e3db20b38caf325
Hidayatullah19061 <64566485+Hidayatullah19061@users.noreply.github.com>
2020-05-14 13:03:51 +0800

    Create jobsheet 1 C++.cpp
    Membuat Program Array Satu Dimensi Menampilkan NIM Masing-masing Mahasiswa

---
+++ jobsheet 1 C++.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int array[11]={12,1,3,4,5,6,7,8,9,0,122};
+	cout<<"Nama : Hidayatullah\nNIM : F1B019061";
+	cout<<"\n\nKelompok : 13";
+	cout<<"\nData ke-1= "<<array[0];
+	cout<<"\nData ke-2= "<<array[1];
+	cout<<"\nData ke-3= "<<array[2];
+	cout<<"\nData ke-4= "<<array[3];
+	cout<<"\nData ke-5= "<<array[4];
+	cout<<"\nData ke-6= "<<array[5];
+	cout<<"\nData ke-7= "<<array[6];
+	cout<<"\nData ke-8= "<<array[7];
+	cout<<"\nData ke-9= "<<array[8];
+	cout<<"\nData ke-10= "<<array[9];
+	cout<<"\nData ke-11= "<<array[10];
+}
