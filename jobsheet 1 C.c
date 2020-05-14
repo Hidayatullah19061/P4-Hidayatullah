68d29fa25a8bd1a1c09f3f2fda1298a1ddf8ee08
Hidayatullah19061 <64566485+Hidayatullah19061@users.noreply.github.com>
2020-05-14 13:00:21 +0800

    Create jobsheet 1 C.c
    Membuat Program Array Satu Dimensi Menampilkan NIM Masing-masing Mahasiswa

---
+++ jobsheet 1 C.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+	int array[11]={12,1,3,4,5,6,7,8,9,0,122};
+	printf("Nama : Hidayatullah\nNIM : F1B019061");
+	printf("\nKelompok : 13");
+	printf("\n\nData ke-1= %d",array[0]);
+	printf("\nData ke-2= %d",array[1]);
+	printf("\nData ke-3= %d",array[2]);
+	printf("\nData ke-4= %d",array[3]);
+	printf("\nData ke-5= %d",array[4]);
+	printf("\nData ke-6= %d",array[5]);
+	printf("\nData ke-7= %d",array[6]);
+	printf("\nData ke-8= %d",array[7]);
+	printf("\nData ke-9= %d",array[8]);
+	printf("\nData ke-10= %d",array[9]);
+	printf("\nData ke-11= %d",array[10]);
+}	
+}
